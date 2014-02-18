4dcc571702d15a2e318cf82104ca4b5add5fb9e0
n.st <nrsend-github@yahoo.de>
2014-02-18 18:47:52 +0100

    Set title on front page

--- header.php
+++ header.php
@@ -11,7 +11,13 @@
 		<!-- Google Chrome Frame for IE -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		<title><?php wp_title(''); ?></title>
+		<title><?php
+			if (is_front_page()) {
+				echo 'IEEE Student Branch Passau';
+			} else {
+				wp_title('');
+			}
+		?></title>
 
 		<!-- mobile meta (hooray!) -->
 		<meta name="HandheldFriendly" content="True">
