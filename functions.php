4c0633e6a8079897dda25f4f3ef0dcb0fb8367b1
Max Klenk <max@klenk.biz>
2014-02-24 16:54:35 +0100

    filter out termine and appointments on home page

--- functions.php
+++ functions.php
@@ -40,6 +40,14 @@ add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
 function add_my_post_types_to_query( $query ) {
 	if ( is_home() && $query->is_main_query() )
 		$query->set( 'post_type', array( 'event', 'post') );
+		$query->set('tax_query', array(
+	        array(
+	            'taxonomy' => 'event-categories',
+	            'field' => 'slug',
+	            'terms' => array('termin', 'appointment'),
+	            'operator' => 'NOT IN'
+	       )
+    	));
 	return $query;
 }
 
